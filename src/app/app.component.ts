6fbdcd5d7ee27d99c52bd461dbd0a1fab5e18c70
IVM DEV <ivmdeveloper@gmail.com>
2021-01-07 20:10:24 -0300

    [CHG] nuevas librerias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'primerAppGen6';
+  title = 'Primer App Angular Gen 6';
 }
