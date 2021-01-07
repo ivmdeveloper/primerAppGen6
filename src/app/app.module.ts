c218b9febff92bb9225a8d24858d45671f9a6359
IVM DEV <ivmdeveloper@gmail.com>
2021-01-07 20:16:02 -0300

    [CHG] nuevos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IconsComponent } from './components/icons/icons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IconsComponent
   ],
   imports: [
     BrowserModule,
